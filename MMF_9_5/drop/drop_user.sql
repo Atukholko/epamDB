15f4b996a41f2f815391ab94f16b544fb03c50a3
Atukhloko <a.tukholko@gmail.com>
2020-04-24 11:53:19 +0300

    fix

--- MMF_9_5/drop/drop_user.sql
+++ MMF_9_5/drop/drop_user.sql
@@ -1,7 +1,5 @@
-DEFINE USER_NAME_DROP = &&1;
+DEFINE USER_NAME = &&1
 
 alter session set "_oracle_script"=true;
 
-DROP USER &&USER_NAME_DROP CASCADE; 
-
-UNDEFINE USER_NAME_DROP;
+DROP USER "&&USER_NAME" CASCADE;
